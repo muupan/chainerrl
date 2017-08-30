muupan/chainerrl

commit 529cc86b88735a9db4d454c781ec6065c530a895

Use StatisticalRecoder in PPO

chainerrl/agents/ppo.py
@@ -8,6 +8,7 @@ import copy
 import numpy as np
 
 
+import chainerrl
 from chainerrl import agent
 from chainerrl.misc.batch_states import batch_states
 
@@ -92,12 +93,12 @@ class PPO(agent.AttributeSavingMixin, agent.Agent):
         self.normalize_advantage = normalize_advantage
         self.logger = logger
 
-        self.average_v = 0
-        self.average_v_decay = average_v_decay
-        self.average_loss_policy = 0
-        self.average_loss_value_func = 0
-        self.average_loss_entropy = 0
-        self.average_loss_decay = average_loss_decay
+        # self.average_v = 0
+        # self.average_v_decay = average_v_decay
+        # self.average_loss_policy = 0
+        # self.average_loss_value_func = 0
+        # self.average_loss_entropy = 0
+        # self.average_loss_decay = average_loss_decay
 
         self.xp = self.model.xp
         self.target_model = None
@@ -106,6 +107,16 @@ class PPO(agent.AttributeSavingMixin, agent.Agent):
         self.memory = []
         self.last_episode = []
         self.n_episodes_in_memory = 0
+
+        self.recorder = chainerrl.statistics_recorder.StatisticsRecorder()
+        self.recorder.register('value', maxlen=1000)
+        self.recorder.register('vf_loss', maxlen=1000)
+        self.recorder.register('policy_loss', maxlen=1000)
+        self.recorder.register('policy_entropy', maxlen=1000)
+        self.recorder.register('policy_kl', maxlen=1000)
+        self.recorder.register('prob_ratio', maxlen=1000)
+        self.recorder.register('value_change', maxlen=1000)
+        self.recorder.register('explained_variance', maxlen=100)
 
     def _act(self, state, train):
         xp = self.xp
@@ -162,8 +173,11 @@ class PPO(agent.AttributeSavingMixin, agent.Agent):
 
         prob_ratio = F.expand_dims(prob_ratio, axis=-1)
         loss_policy = - F.mean(F.minimum(
-            prob_ratio * advs,
-            F.clip(prob_ratio, 1-self.clip_eps, 1+self.clip_eps) * advs))
+            prob_ratio * (advs + vs_pred_old - vs_pred.data),
+            F.clip(prob_ratio, 1 - self.clip_eps, 1 + self.clip_eps) * advs))
+        self.recorder.record('prob_ratio', prob_ratio.data)
+        kl = target_distribs.kl(distribs)
+        self.recorder.record('policy_kl', kl.data)
 
         if self.clip_eps_vf is None:
             loss_value_func = F.mean_squared_error(vs_pred, vs_teacher)
@@ -174,20 +188,15 @@ class PPO(agent.AttributeSavingMixin, agent.Agent):
                                  vs_pred_old - self.clip_eps_vf,
                                  vs_pred_old + self.clip_eps_vf)
                          - vs_teacher)
-                ))
+            ))
+        self.recorder.record('value_change', vs_pred.data - vs_pred_old)
 
         loss_entropy = -F.mean(ent)
 
         # Update stats
-        self.average_loss_policy += (
-            (1 - self.average_loss_decay) *
-            (float(loss_policy.data) - self.average_loss_policy))
-        self.average_loss_value_func += (
-            (1 - self.average_loss_decay) *
-            (float(loss_value_func.data) - self.average_loss_value_func))
-        self.average_loss_entropy += (
-            (1 - self.average_loss_decay) *
-            (float(loss_entropy.data) - self.average_loss_entropy))
+        self.recorder.record('policy_loss', float(loss_policy.data))
+        self.recorder.record('vf_loss', float(loss_value_func.data))
+        self.recorder.record('policy_entropy', -float(loss_entropy.data))
 
         return (
             loss_policy
@@ -202,6 +211,12 @@ class PPO(agent.AttributeSavingMixin, agent.Agent):
         target_model = copy.deepcopy(self.model)
         dataset_iter = chainer.iterators.SerialIterator(
             self.memory, self.minibatch_size)
+
+        # Compute explained variance
+        if len(self.memory) > 0:
+            adv_var = np.var([b['adv'] for b in self.memory])
+            ret_var = np.var([b['v_teacher'] for b in self.memory])
+            self.recorder.record('explained_variance', 1 - adv_var / ret_var)
 
         if self.normalize_advantage:
             advs = np.asarray([float(b['adv']) for b in self.memory])
@@ -230,15 +245,13 @@ class PPO(agent.AttributeSavingMixin, agent.Agent):
                 advs=xp.array([b['adv'] for b in batch], dtype=xp.float32),
                 vs_teacher=xp.array(
                     [b['v_teacher'] for b in batch], dtype=xp.float32),
-                )
+            )
 
     def act_and_train(self, state, reward):
         action, v = self._act(state, train=False)
 
         # Update stats
-        self.average_v += (
-            (1 - self.average_v_decay) *
-            (float(v) - self.average_v))
+        self.recorder.record('value', float(v))
 
         if self.last_state is not None:
             self.last_episode.append({
@@ -259,10 +272,11 @@ class PPO(agent.AttributeSavingMixin, agent.Agent):
     def act(self, state):
         action, v = self._act(state, train=False)
 
+        self.recorder.record('value', float(v))
         # Update stats
-        self.average_v += (
-            (1 - self.average_v_decay) *
-            (float(v) - self.average_v))
+        # self.average_v += (
+        #     (1 - self.average_v_decay) *
+        #     (float(v) - self.average_v))
 
         return action
 
@@ -291,12 +305,7 @@ class PPO(agent.AttributeSavingMixin, agent.Agent):
         pass
 
     def get_statistics(self):
-        return [
-            ('average_v', self.average_v),
-            ('average_loss_policy', self.average_loss_policy),
-            ('average_loss_value_func', self.average_loss_value_func),
-            ('average_loss_entropy', self.average_loss_entropy),
-        ]
+        return self.recorder.get_statistics()
 
 
 class ParallelPPO(PPO):
@@ -379,7 +388,8 @@ class ParallelPPO(PPO):
             self.memory.extend(seq)
             self.n_episodes_in_memory += 1
             self.last_episode[i] = []
-            self.logger.debug('memory extend: %s + %s = %s n_episodes_in_memory: %s', old_memory_size, len(seq), len(self.memory), self.n_episodes_in_memory)
+            self.logger.debug('memory extend: %s + %s = %s n_episodes_in_memory: %s',
+                              old_memory_size, len(seq), len(self.memory), self.n_episodes_in_memory)
 
     def _batch_compute_teacher(self, finished_episodes_only=False):
         """Estimate state values and advantages of self.last_episode

chainerrl/__init__.py
@@ -18,6 +18,7 @@ from chainerrl import q_function  # NOQA
 from chainerrl import q_functions  # NOQA
 from chainerrl import recurrent  # NOQA
 from chainerrl import replay_buffer  # NOQA
+from chainerrl import statistics_recorder  # NOQA
 from chainerrl import v_function  # NOQA
 from chainerrl import v_functions  # NOQA
 from chainerrl import wrappers  # NOQA

chainerrl/statistics_recorder.py
@@ -1,0 +1,64 @@
+import collections
+
+import chainer
+import numpy as np
+
+
+def max_or_none(xs):
+    if xs:
+        return np.max(xs)
+    else:
+        return np.nan
+
+
+def min_or_none(xs):
+    if xs:
+        return np.min(xs)
+    else:
+        return np.nan
+
+
+class StatisticsRecorder(object):
+
+    def __init__(self):
+        self.records = {}
+        self.statistic_types = {}
+        self.counts = {}
+
+    def register(self, key, maxlen=100,
+                 statistic_types=[
+                     'population',
+                     'mean',
+                     'std',
+                     'max',
+                     'min',
+                     'count']):
+        assert key not in self.records
+        self.records[key] = collections.deque(maxlen=maxlen)
+        self.statistic_types[key] = statistic_types
+        self.counts[key] = 0
+
+    def record(self, key, value):
+        if np.isscalar(value):
+            self.records[key].append(value)
+            self.counts[key] += 1
+        else:
+            self.records[key].extend(chainer.cuda.to_cpu(value.ravel()))
+            self.counts[key] += value.size
+
+    def get_statistics(self):
+        keys = sorted(self.records)
+        statistics = []
+        for key in keys:
+            for stat_type in self.statistic_types[key]:
+                stat_key = key + '/' + stat_type
+                stat_val = {
+                    'population': lambda: len(self.records[key]),
+                    'mean': lambda: np.mean(self.records[key]),
+                    'std': lambda: np.std(self.records[key]),
+                    'max': lambda: max_or_none(self.records[key]),
+                    'min': lambda: min_or_none(self.records[key]),
+                    'count': lambda: self.counts[key],
+                }[stat_type]()
+                statistics.append((stat_key, stat_val))
+        return statistics

commit 7a2cd2011dd339569a635d2cbb7f3aa1a1e3d314

Fix a bug introduced by 529cc86b88735a9db4d454c781ec6065c530a895
Results produced from 529cc86b88735a9db4d454c781ec6065c530a895 to this
commit are not correct.

chainerrl/agents/ppo.py
@@ -173,7 +173,7 @@ class PPO(agent.AttributeSavingMixin, agent.Agent):
 
         prob_ratio = F.expand_dims(prob_ratio, axis=-1)
         loss_policy = - F.mean(F.minimum(
-            prob_ratio * (advs + vs_pred_old - vs_pred.data),
+            prob_ratio * advs,
             F.clip(prob_ratio, 1 - self.clip_eps, 1 + self.clip_eps) * advs))
         self.recorder.record('prob_ratio', prob_ratio.data)
         kl = target_distribs.kl(distribs)