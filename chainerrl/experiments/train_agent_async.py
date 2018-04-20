2457249a86ee2564c651aff022e200896bb37c8b
muupan <muupan@gmail.com>
2018-04-20 21:57:33 +0900

    Add eval_max_episode_len to train_agent_async

--- chainerrl/experiments/train_agent_async.py
+++ chainerrl/experiments/train_agent_async.py
@@ -132,6 +132,7 @@ def train_agent_async(outdir, processes, make_env,
                       step_offset=0,
                       successful_score=None,
                       eval_explorer=None,
+                      eval_max_episode_len=None,
                       agent=None,
                       make_agent=None,
                       global_step_hooks=[],
@@ -156,6 +157,8 @@ def train_agent_async(outdir, processes, make_env,
         successful_score (float): Finish training if the mean score is greater
             or equal to this value if not None
         eval_explorer: Explorer used for evaluation.
+        eval_max_episode_len (int or None): Maximum episode length of
+            evaluation runs. If set to None, max_episode_len is used instead.
         agent (Agent): Agent to train.
         make_agent (callable): (process_idx) -> Agent
         global_step_hooks (list): List of callable objects that accepts
@@ -175,6 +178,9 @@ def train_agent_async(outdir, processes, make_env,
     # Prevent numpy from using multiple threads
     os.environ['OMP_NUM_THREADS'] = '1'
 
+    if eval_max_episode_len is None:
+        eval_max_episode_len = max_episode_len
+
     counter = mp.Value('l', 0)
     episodes_counter = mp.Value('l', 0)
     training_done = mp.Value('b', False)  # bool
@@ -192,7 +198,7 @@ def train_agent_async(outdir, processes, make_env,
         evaluator = AsyncEvaluator(
             n_runs=eval_n_runs,
             eval_interval=eval_interval, outdir=outdir,
-            max_episode_len=max_episode_len,
+            max_episode_len=eval_max_episode_len,
             step_offset=step_offset,
             explorer=eval_explorer,
             save_best_so_far_agent=save_best_so_far_agent,
